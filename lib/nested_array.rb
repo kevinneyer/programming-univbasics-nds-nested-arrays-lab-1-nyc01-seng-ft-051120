ac347545dcc8b5b4cd42e0524d04aa4468def5a7
Kevin Neyer <kevinneyer@gmail.com>
2020-04-11 17:17:42 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -50,7 +50,7 @@ def matrix_lookup(matrix, row, column)
     CONVENTIONAL_PRODUCE,
     ORGANIC_PRODUCE
    ]
-    storage_room
+    storage_room[1][1]
 end
 
 def matrix_update(matrix, row, column, new_value)
